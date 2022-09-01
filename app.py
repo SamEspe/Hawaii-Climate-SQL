563bde1005568f8a0512d12fe433b4b4b895a57f
Sam Espe <sam.espe.sci@gmail.com>
2022-09-01 18:41:38 -0500

    Fixed issue with x-labels on precipitation graph. Started adding Flask content to app.py

--- app.py
+++ app.py
@@ -1,5 +1,27 @@
 # Import dependencies
-from flask import Flask
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 # Create app
 app = Flask(__name__)
@@ -14,13 +36,23 @@ def homepage():
 
 # Precipitation
 
+@app.route('/api/v1.0/precipitation')
+def precipitation:
+
+
 # Stations
 
+@app.route('/api/v1.0/stations')
+def stations:
+
 # Temperature Observations
+@app.route('/api/v1.0/tobs')
 
 # Temperature - all dates after "start"
+@app.route('/api/v1.0/<start>')
 
 # Temperature - date range
+@app.route('/api/v1.0/<start>/<end>')
 
 # Run Flask server
 if __name__ == "__main__":
